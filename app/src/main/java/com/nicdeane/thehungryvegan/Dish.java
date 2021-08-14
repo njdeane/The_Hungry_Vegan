643a01aec7d5c0015b6fd79b845a8b83db16b3d5
njdeane <njdeane7@gmail.com>
2021-08-14 17:19:14 +1000

    Build out dish class. Replace array in main with new array of dish objects.

--- app/src/main/java/com/nicdeane/thehungryvegan/Dish.java
+++ app/src/main/java/com/nicdeane/thehungryvegan/Dish.java
@@ -1,5 +1,18 @@
 package com.nicdeane.thehungryvegan;
 
 public class Dish {
+    String title;
+    String description;
+    int price;
 
+    Dish(String title, String description, int price) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
 }
--- app/src/main/java/com/nicdeane/thehungryvegan/StartersActivity.java
+++ app/src/main/java/com/nicdeane/thehungryvegan/StartersActivity.java
@@ -16,30 +16,28 @@ public class StartersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_starters);
 
         ListView startersList = findViewById(R.id.list_view_starters);
-        String[] startersArray = {
-                "Mushroom and tofu maki",
-                "Egg and avocado uramaki",
-                "Melon and lemon soup",
-                "Coconut and chocolate mousse",
-                "Spinach and cabbage wontons",
-                "Broccoli and cucumber soup",
-                "Chilli and aubergine dip",
-                "Chickpea and chilli gyoza",
-                "Sprout and pineapple soup",
-                "Egusi and borscht soup",
-                "Aubergine and egg sushi",
-                "Artichoke and mustard soup",
-                "Peppercorn and tamarind soup",
-                "Parsley and celeriac parcels",
-                "Pasta and broccoli soup",
-                "Potato and courgette soup",
-                "Chickpea and cabbage parcels",
-                "Coriander and peppercorn gyoza",
-                "Pear and chestnut soup",
-                "Pesto and garam masala parcels"
+
+        Dish[] dishArray = {
+                new Dish("Melon and lemon soup", "Fresh melon and lemon combined into creamy soup", 1199),
+                new Dish("Coconut and chocolate mousse", "A creamy mousse made with fresh coconut and milk chocolate", 899),
+                new Dish("Spinach and cabbage wontons", "Thin wonton cases stuffed with fresh spinach and chinese cabbage", 799),
+                new Dish("Broccoli and cucumber soup", "Fresh broccoli and cucumber combined into creamy soup", 899),
+                new Dish("Chilli and aubergine dip", "A dip made from scotch bonnet chilli and fresh aubergine", 999),
+                new Dish("Chickpea and chilli Beans", "Thin pastry cases stuffed with fresh chickpea and green chilli", 699),
+                new Dish("Sprout and pineapple soup", "Fresh sprout and pineapple combined into creamy soup", 899),
+                new Dish("Pie and borscht soup", "Pie and borscht combined into creamy soup", 1299),
+                new Dish("Melon and lemon soup", "Fresh melon and lemon combined into creamy soup", 1199),
+                new Dish("Coconut and chocolate mousse", "A creamy mousse made with fresh coconut and milk chocolate", 899),
+                new Dish("Spinach and cabbage wontons", "Thin wonton cases stuffed with fresh spinach and chinese cabbage", 799),
+                new Dish("Broccoli and cucumber soup", "Fresh broccoli and cucumber combined into creamy soup", 899),
+                new Dish("Chilli and aubergine dip", "A dip made from scotch bonnet chilli and fresh aubergine", 999),
+                new Dish("Chickpea and chilli beans", "Thin pastry cases stuffed with fresh chickpea and green chilli", 699),
+                new Dish("Sprout and pineapple soup", "Fresh sprout and pineapple combined into creamy soup", 899),
+                new Dish("Beans and borscht soup", "Beans and borscht combined into creamy soup", 1299)
         };
 
-        ArrayAdapter<String> dishesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, startersArray);
+
+        ArrayAdapter<Dish> dishesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dishArray);
         startersList.setAdapter(dishesAdapter);
 
 
